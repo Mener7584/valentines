0b07390a791e420e74bd9d2fb1082d79a4e1b6a2
Mener7584 <138903594+Mener7584@users.noreply.github.com>
2025-02-13 18:23:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+function itemInViewport(value, scrollOffset) {
+    const item = value.getBoundingClientRect();
+    const offset = parseFloat(scrollOffset) || 0;
+
+    return item.top <= window.innerHeight * (1 - offset) && item.bottom >= window.innerHeight * offset;
+}
+
+const elements = document.querySelectorAll("[data-animation]");
+
+function elementAnimation() {
+    elements.forEach((element) => {
+        if (itemInViewport(element, element.getAttribute("data-animation-offset"))) {
+            element.classList.add("inViewport");
+        } else if (element.hasAttribute("data-animation-repeat")) {
+            element.classList.remove("inViewport");
+        }
+    });
+}
+
+elementAnimation();
+
+function onScrollOrResize() {
+    requestAnimationFrame(elementAnimation);
+}
+
+window.addEventListener("scroll", onScrollOrResize);
+window.addEventListener("resize", onScrollOrResize);
